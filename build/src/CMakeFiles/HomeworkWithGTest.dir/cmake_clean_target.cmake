e666a33a2506b168f608f73fcdeb529267ef0aef
Time has wings <linguangming@hust.edu.cn>
2024-10-15 08:56:29 +0800

    add gitignore && remove cached

--- build/src/CMakeFiles/HomeworkWithGTest.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "D:/VSCodeProjects/C++Course/HomeworkWithGTest/bin/libHomeworkWithGTest.a"
-)
--- build/tests/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/VSCodeProjects/C++Course/HomeworkWithGTest/tests/googletest
-# Build directory: D:/VSCodeProjects/C++Course/HomeworkWithGTest/build/tests/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
